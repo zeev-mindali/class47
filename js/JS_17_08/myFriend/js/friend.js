c035d3c5056f5a0012704f938ba19b7dc355962c
Zeev mindali <mindalizeev@gmail.com>
2023-08-17 21:57:18 +0200

    back tick

--- js/JS_17_08/myFriend/js/friend.js
+++ js/JS_17_08/myFriend/js/friend.js
@@ -1,4 +1,7 @@
 function loadImage(){
     var img = document.getElementById("imageURL").value;
-    document.getElementById("friendImage").src = img;
+    var friendName = document.getElementById("friendName").value;
+    var friendTel = document.getElementById("friendTel").value;
+    //document.getElementById("friendImage").src = img;
+    document.getElementById("friendList").innerHTML=`<tr><td><img src="${img}" width="50"/></td>${friendName}<td></td><td>${friendTel}</td>`;
 }
