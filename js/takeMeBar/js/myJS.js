e7d12dade641a88fee15ee6f4515c099c8808a1b
Zeev mindali <mindalizeev@gmail.com>
2023-08-31 18:39:45 +0200

    fix to two hr in node child

--- js/takeMeBar/js/myJS.js
+++ js/takeMeBar/js/myJS.js
@@ -5,6 +5,7 @@ var init=()=>{
     
     setInterval(()=>{
         //window.location.reload;
+        initVictim();
         showVictim();
     },1000)
 }
@@ -23,13 +24,34 @@ var showList=()=>{
 var showVictim=()=>{
     //console.log(randomGirl(girls.length));
     var victim= document.querySelector("#victim");
+    var speartor = document.querySelector(("#speartor"))
     var hr = document.createElement("hr");
     var h1 = document.createElement("h1");
     h1.textContent = girls[randomGirl(girls.length)];
+    speartor.appendChild(h1);
     victim.appendChild(hr);
-    victim.appendChild(h1)
+    victim.appendChild(h1);
 }
 
 var randomGirl=(max)=>{
     return Math.floor(Math.random()*max);
+}
+
+//stupid but working method
+// var initVictim=()=>{
+//     var victim= document.querySelector("#victim");
+//     victim.innerHTML="";
+// }
+
+var initVictim=()=>{
+    //select the id by selector
+    var victim= document.querySelector("#victim");
+    //select the first child
+    var first = victim.firstElementChild;
+    while (first){
+       first.remove();
+       first = victim.firstElementChild;
+    }
+    
+    
 }
